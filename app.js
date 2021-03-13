9fd315a48fb2294cbed786cdc54a3d269864e806
AbramFernandoH <abramfernando18@gmail.com>
2021-03-13 13:41:44 +0700

    Styling component and adding date on exchange currency

--- app.js
+++ app.js
@@ -4,25 +4,32 @@ const exRatesBtn = document.getElementById('exchange-rates');
 exRatesBtn.addEventListener('click', function(evt){
   evt.preventDefault();
   let result = document.querySelector('.show-rates .result');
+  let dateToday = document.querySelector('.show-rates .dateToday');
   result.innerHTML = '';
+  dateToday.innerHTML = '';
 
   let baseAmount = document.querySelector('#baseAmount');
   let baseCurrency = document.querySelector('#base');
   let convertCurrency = document.querySelector('#convert');
 
+  if(baseAmount.value === ''){
+    alert('Fill the amount of base currency');
+    return
+  } 
+
   axios.get(`${exchangeForm.getAttribute('action')}base=${baseCurrency.value}`)
   .then(exRates => {
-    let { base, rates } = exRates.data;
+    let { base, rates, date } = exRates.data;
     fx.base = base;
     fx.rates = rates;
-    console.log(exRates.data);
+    fx.date = date;
   })
   .then(() => {
-    let { base } = fx;
+    let { base, date } = fx;
     let amount = parseFloat(baseAmount.value);
     let rate = fx(amount).from(base).to(convertCurrency.value);
-    console.log(`${amount} ${base} = ${rate.toFixed(2)} ${convertCurrency.value}`);
     result.innerHTML = `${amount} ${base} = ${rate.toFixed(2)} ${convertCurrency.value}`;
+    dateToday.innerHTML = `According to rates at ${date}`;
   })
   .catch(err => {
     console.log('error!!!');
@@ -30,4 +37,3 @@ exRatesBtn.addEventListener('click', function(evt){
   })
 })
 
-// https://api.exchangeratesapi.io/latest?base=USD

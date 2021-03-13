6709074ddec4b6c7c143c668709d8eed45e6ffc2
AbramFernandoH <abramfernando18@gmail.com>
2021-03-13 12:35:02 +0700

    Successfully exchange rates from and to currency rates that been choosen by user. And display it to web page

--- app.js
+++ app.js
@@ -1,23 +1,33 @@
+const exchangeForm = document.getElementById('exchange-form');
 const exRatesBtn = document.getElementById('exchange-rates');
 
-exRatesBtn.addEventListener('click', function(e){
-  e.preventDefault;
-})
+exRatesBtn.addEventListener('click', function(evt){
+  evt.preventDefault();
+  let result = document.querySelector('.show-rates .result');
+  result.innerHTML = '';
 
+  let baseAmount = document.querySelector('#baseAmount');
+  let baseCurrency = document.querySelector('#base');
+  let convertCurrency = document.querySelector('#convert');
 
-axios.get('https://api.exchangeratesapi.io/latest?base=USD')
+  axios.get(`${exchangeForm.getAttribute('action')}base=${baseCurrency.value}`)
   .then(exRates => {
     let { base, rates } = exRates.data;
     fx.base = base;
     fx.rates = rates;
-    console.log(fx.base, fx.rates);
+    console.log(exRates.data);
   })
   .then(() => {
     let { base } = fx;
-    let rate = fx(2).from(base).to('JPY');
-    console.log(`2 ${base} = ${rate.toFixed(2)} JPY`);
+    let amount = parseFloat(baseAmount.value);
+    let rate = fx(amount).from(base).to(convertCurrency.value);
+    console.log(`${amount} ${base} = ${rate.toFixed(2)} ${convertCurrency.value}`);
+    result.innerHTML = `${amount} ${base} = ${rate.toFixed(2)} ${convertCurrency.value}`;
   })
   .catch(err => {
     console.log('error!!!');
     console.log(err);
   })
+})
+
+// https://api.exchangeratesapi.io/latest?base=USD
